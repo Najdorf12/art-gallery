a83bb05cf847ae1c18f73a5794d8a947c154d7f1
Najdorf <agustin.morro@gmail.com>
2025-06-29 18:30:22 -0300

    add styles

--- src/components/CardsHome.jsx
+++ src/components/CardsHome.jsx
@@ -5,12 +5,12 @@ import arrow from "/arrow.png";
 const CardsHome = ({ artist }) => {
   return (
     <Link to={`/Artista/${artist?.id}`}>
-      <div className="relative z-50 border-l-[2px] border-b-[2px] border-stone-600 w-[180px] rounded-sm h-[120px] pl-3 pt-2 lg:pl-3 lg:w-[220px] lg:h-[133px] xl:w-[280px] xl:h-[154px]  2xl:w-[280px] 2xl:h-[178px] hover:border-stone-300 duration-700 cursor-pointer group dark:border-stone-300 hover:-translate-y-6 ">
+      <div className="relative z-50 border-l-[2px] border-b-[2px] border-stone-600 w-[180px] rounded-sm h-[120px] pl-3 pt-2 lg:pl-3 lg:w-[220px] lg:h-[133px] xl:w-[280px] xl:h-[154px]  2xl:w-[280px] 2xl:h-[178px] hover:border-stone-300 duration-1000 cursor-pointer group dark:border-stone-300 hover:-translate-y-6 ">
         <figure>
           <img
             src={artist?.icon}
             alt="icon"
-            className="w-10 h-10 object-cover object-center rounded-full lg:w-12 lg:h-12 xl:h-14 xl:w-14 2xl:h-16 2xl:w-16 group-hover:scale-120 duration-700 border border-stone-400 group-hover:translate-x-[200px]"
+            className="w-10 h-10 object-cover object-center rounded-full lg:w-12 lg:h-12 xl:h-14 xl:w-14 2xl:h-16 2xl:w-16 group-hover:scale-120 duration-1000 border border-stone-400 group-hover:translate-x-[200px]"
           />
         </figure>
         <article className="text-sm mt-4  md:text-balance lg:text-base xl:mt-5 xl:text-lg 2xl:mt-7  2xl:text-lg">
@@ -22,7 +22,7 @@ const CardsHome = ({ artist }) => {
             {artist?.contentHome}
           </p>
         </article>
-        <figure className="w-6 md:w-7 p-1 absolute bottom-1 -right-2 md:bottom-1 md:-right-2 -rotate-45 group-hover:translate-x-3 group-hover:rotate-0 duration-300">
+        <figure className="w-6 md:w-7 p-1 absolute bottom-1 -right-2 md:bottom-1 md:-right-2 -rotate-45 group-hover:translate-x-3 group-hover:rotate-0 duration-1000">
           <img src={arrowOrange} alt="" />
         </figure>
       </div>{" "}
