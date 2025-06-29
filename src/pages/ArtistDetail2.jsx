96974bc2119aceda1240f4cfe9618cf3d4916925
Najdorf <agustin.morro@gmail.com>
2025-06-29 18:07:59 -0300

    add styles

--- src/pages/ArtistDetail2.jsx
+++ src/pages/ArtistDetail2.jsx
@@ -4,7 +4,7 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 import Navbar from "../components/Navbar";
 import { Link, useParams } from "react-router-dom";
 import arrow from "/arrow-orange.png";
-import BtnPrimary from "../components/BtnPrimary";
+import ThemeToggle from "../components/ThemeToggle";
 import { ReactLenis } from "lenis/react";
 import Slider from "../components/Slider";
 
@@ -77,12 +77,11 @@ const ArtistDetail = ({ artistsData }) => {
   return (
     <>
       <ReactLenis root ref={lenisRef} autoRaf={false} />
-      <section className="bg-whiteCustom w-full font-text2 overflow-hidden">
+      <section className="bg-blackCustom w-full font-text2 overflow-hidden ">
         <Navbar />
-
         <section className="relative w-full h-screen flex flex-col md:flex-row">
-          <section className="w-full h-[70vh] pl-[5%] md:h-[90vh] xl:pl-0 md:w-[55%] bg-whiteCustom flex flex-col items-center justify-center">
-            <article>
+          <section className="w-full h-[70vh] pl-[5%] md:h-screen xl:pl-0 md:w-[55%] bg-whiteCustom  flex flex-col items-center justify-center">
+            <article className="">
               <h6 className="font-title -mt-16 text-8xl md:text-8xl lg:text-[9rem]  xl:text-[12rem] xl:leading-[12rem] text-stone-300">
                 {artist.firstname + " "}{" "}
                 <span className="text-orangeCustom">{artist.lastname}</span>
@@ -105,10 +104,9 @@ const ArtistDetail = ({ artistsData }) => {
             </article>
           </section>
 
-          <section className="w-full h-[30vh] md:w-1/2 md:h-screen flex flex-col items-end justify-end">
-            <div className="w-[30%] md:w-[90%] h-[1vh] bg-grayCustom "></div>
-            <div className="w-[85%] md:w-full h-[40vh] bg-orangeCustom flex justify-center items-end">
-              <p className="font-title text-whiteCustom text-5xl md:text-7xl text-end text-balance pb-6 px-3 md:px-8 xl:pb-7 ">
+          <section className="w-full h-[30vh] md:w-1/2  md:h-screen flex flex-col items-end justify-end">
+            <div className="w-[85%] md:w-full h-[40vh] rounded-tl-lg  flex justify-center items-end">
+              <p className="font-title text-orangeCustom text-5xl md:text-7xl text-end text-balance pb-6 px-3 md:px-8 xl:pb-7 ">
                 "{artist.quote1}"
               </p>
             </div>
@@ -116,11 +114,11 @@ const ArtistDetail = ({ artistsData }) => {
         </section>
 
         {/* DESCRIPTION3 */}
-        <section className="w-full relative py-12 md:py-24 text-stone-300 bg-blackCustom flex justify-end items-center">
+        <section className="w-full relative py-12 md:py-24 text-stone-300 bg-blackCustom  flex justify-end items-center">
           <div className="w-[90%] relative z-50 self-end text-balance text-grayCustom text-sm px-4 lg:pr-32  lg:w-[70%] lg:text-base">
             {artist.description2}
           </div>
-          <div className="absolute left-0 z-5 bg-whiteCustom w-[5%] h-full lg:w-[25%]"></div>
+          <div className="absolute left-0 z-5 bg-blackCustom w-[5%] h-full lg:w-[25%]"></div>
         </section>
 
         {/* Images Section */}
@@ -159,14 +157,16 @@ const ArtistDetail = ({ artistsData }) => {
                       ref={textRef}
                       className="pl-1 md:pl-12 md:pt-32 relative"
                     >
-                     {/*  <div className="w-[100%] h-[1px] bg-orangeCustom absolute -bottom-2 md:-bottom-9"></div>
+                      {/*  <div className="w-[100%] h-[1px] bg-orangeCustom absolute -bottom-2 md:-bottom-9"></div>
                       <div className="w-[30%] h-[1px] bg-orangeCustom absolute -top-5 md:top-12"></div> */}
                       <h6 className="text-6xl leading-10 font-title text-whiteCustom md:leading-24 md:text-8xl ">
                         {selectedImage.name}
                       </h6>
                       <ul className="mt-8 flex flex-col gap-2 text-stone-500 text-sm md:text-base md:mt-16 md:gap-3">
                         <li>{selectedImage.description.tecnica}</li>
-                        <li className="font-text3">{selectedImage.description.medidas} cm</li>
+                        <li className="font-text3">
+                          {selectedImage.description.medidas} cm
+                        </li>
                         <li>{selectedImage.description.autor}</li>
                       </ul>
                     </div>
@@ -176,8 +176,6 @@ const ArtistDetail = ({ artistsData }) => {
             </div>
           )}
         </section>
-
-    
       </section>
     </>
   );
